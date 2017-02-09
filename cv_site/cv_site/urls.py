a09d5d4a50539ed5dce089ddd7cc6141ba1f1654
Khushmeet <khushmeetsingh1996@gmail.com>
2017-02-09 22:11:14 +0530

    cv detect method

--- cv_site/cv_site/urls.py
+++ cv_site/cv_site/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from face_recog.views import detect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^face_recog/detect/$', detect, name='detect')
 ]
